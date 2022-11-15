33bba9e669fcc46895415b9c78c8c5d4a2b2b7d5
Gautam Banuru <gbanuru@gmail.com>
2022-11-14 18:47:44 -0800

    first 3 test apps

---
+++ app2/app.py
@@ -0,0 +1,13 @@
+import os
+import time
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return f"APP2: Time is {time.ctime()} in Process ID {os.getpid()}\n"
+
+if __name__ == "__main__":
+   app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
