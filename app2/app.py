f2776a6b8d0b276959ca546dc8c8300db785699c
Gautam Banuru <gbanuru@gmail.com>
2022-11-18 19:09:10 -0800

    added init + execution time sleep delays to apps

--- app2/app.py
+++ app2/app.py
@@ -1,5 +1,6 @@
 import os
 import time
+import random
 
 from flask import Flask
 
@@ -7,6 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+   # Sleep for 56 seconds + random time between 0 and 2 second
+   time.sleep(56 + random.uniform(0, 2))
+
    return f"APP2: Time is {time.ctime()} in Process ID {os.getpid()}\n"
 
 if __name__ == "__main__":
--- app5/app.py
+++ app5/app.py
@@ -1,5 +1,6 @@
 import os
 import time
+import random
 
 from flask import Flask
 
@@ -7,6 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+   # Sleep for 2 seconds + random time between 0 and 2 second
+   time.sleep(2 + random.uniform(0, 2))
+
    return f"APP5: Time is {time.ctime()} in Process ID {os.getpid()}\n"
 
 if __name__ == "__main__":
