f11e8b0cc9ef2a017f76629a8ce34b9bd4bb5bd8
Gautam Banuru <gbanuru@gmail.com>
2022-11-20 14:11:30 -0800

    added poisson distribution, use option 1

--- run_perf.sh
+++ run_perf.sh
@@ -6,19 +6,37 @@ import sys
 import subprocess
 import time
 
+OPTION = 1
+
 def process_arguments():
     LOAD_CONFIG = ""
-    # take command line argument for --spike or -S, --poisson or -P, --exponential or -E
-    if len(sys.argv) == 2:
+    # take command line argument for --spike or -S, --poisson or -P, --exponential or -E, --new_poisson or -N
+    if len(sys.argv) >= 2:
         if sys.argv[1] == "--spike" or sys.argv[1] == "-S":
             LOAD_CONFIG = "config/load/spike.json"
         elif sys.argv[1] == "--linear" or sys.argv[1] == "-P":
             LOAD_CONFIG = "config/load/poisson.json"
         elif sys.argv[1] == "--exponential" or sys.argv[1] == "-E":
             LOAD_CONFIG = "config/load/exponential.json"
+        elif sys.argv[1] == "--new_poisson" or sys.argv[1] == "-N":
+            LOAD_CONFIG = "config/load/new_poisson.json"
+        else:
+            print("Invalid argument. Please use --spike or -S, --linear or -P, --exponential or -E, --new_poisson or -N")
+            sys.exit(1)
+    
+    if len(sys.argv) == 3:
+        if sys.argv[2] == "--option1" or sys.argv[2] == "-O1":
+            OPTION = 1
+        elif sys.argv[2] == "--option2" or sys.argv[2] == "-O2":
+            OPTION = 2
+        elif sys.argv[2] == "--option3" or sys.argv[2] == "-O3":
+            OPTION = 3
+        else:
+            print("Invalid argument. Please use --option1 or -O1, --option2 or -O2, --option3 or -O3")
+            sys.exit(1)
     
     if not os.path.isfile(LOAD_CONFIG):
-        print("\nPlease specify a valid test type: --spike (-S), --poisson (-P), or --exponential (-E)\n")
+        print("\nPlease specify a valid test type: --spike (-S), --poisson (-P), or --exponential (-E), or --new_poisson (-N)\n")
         exit(1)
     
     return LOAD_CONFIG
@@ -88,10 +106,13 @@ def run_perf(config):
             # send output to file
             with open(f"result/{endpoint_name}_{config['LOAD_TYPE']}.txt","a") as out:
                 # hey -n 10 -c 1 -m 'GET' -host "app1-fw1-1.default.127.0.0.1.nip.io"  "http://localhost:8080"
-                # call = f"hey -n {request_count} -c 1 -q {qps} -t 20 -m 'GET' -host '{host}' '{true_endpoint}'"
-                # call = f"hey -z {config['WINDOW_SIZE']}s -c 1 -t 20 -q {qps} -m 'GET' -host '{host}' '{true_endpoint}'"
-                call = f"hey -z {config['WINDOW_SIZE']}s -c {qps} -t 20 -m 'GET' -host '{host}' '{true_endpoint}'"
-                print(f"Request: {call}")
+                if OPTION == 1: # mains average of 250 requests
+                    call = f"hey -n {int(request_count*config['WINDOW_SIZE'])} -c 10 -q {int(request_count / 10)} -t 20 -m 'GET' -host '{host}' '{true_endpoint}'"
+                if OPTION == 2:
+                    call = f"hey -z {config['WINDOW_SIZE']}s -c 1 -t 20 -q {qps} -m 'GET' -host '{host}' '{true_endpoint}'"
+                if OPTION == 3: # used to load test
+                    call = f"hey -z {config['WINDOW_SIZE']}s -c {qps} -t 20 -m 'GET' -host '{host}' '{true_endpoint}'"
+                print(f"Option {OPTION}, Request: {call}")
                 promise = subprocess.Popen(call, shell=True, stdout=out)  
                 promise_list.append(promise)
         time.sleep(config["WINDOW_SIZE"])
